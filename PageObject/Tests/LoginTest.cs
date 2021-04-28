d433fbdf0edb0eb7602f7e3a5477bb759aac2cee
Alexandr Trostyanko <Aleksandr.Trostyanko@itechart-group.com>
2021-04-28 15:16:55 +0300

    Prepare for CircleCI

--- PageObject/Tests/LoginTest.cs
+++ PageObject/Tests/LoginTest.cs
@@ -15,56 +15,13 @@ namespace PageObject
     {
         [Test]
         [SmokeTest]
-        public void Test1()
-        {
-            LoginSteps loginSteps = new LoginSteps(Driver);
-            loginSteps.LogIn();
-            
-            Driver.Navigate().GoToUrl("https://aqa04onl01.testrail.io/index.php?/admin/overview");
-            new DashboardPage(Driver, true);
-            
-            Assert.IsTrue(new DashboardPage(Driver).IsPageOpened());
-            Assert.AreEqual(Driver.Title, "All Projects - TestRail");
-            
-            Console.Out.WriteLine(Project.Name);
-            Console.Out.WriteLine(Project.Type);
-        }
-
-        [Test]
-        [Regression]
         public void Test2()
         {
-            LoginSteps loginSteps = new LoginSteps(Driver);
+            var loginSteps = new LoginSteps(Driver);
             loginSteps.LogIn();
             
             Assert.IsTrue(new DashboardPage(Driver).IsPageOpened());
             Assert.AreEqual(Driver.Title, "All Projects - TestRail");
-        }
-        
-        [Test]
-        [SmokeTest]
-        public void Test3()
-        {
-            LoginSteps loginSteps = new LoginSteps(Driver);
-            loginSteps.LogIn();
-
-            var start = DateTime.Now;
-            var element = new UIElement(Driver, _waitService.GetVisibleElement(By.Id("sidebar-projects-add")));
-            var stop = DateTime.Now;
-            Console.Out.WriteLine( stop.Ticks - start.Ticks);
-        }
-
-        [Test]
-        [SmokeTest]
-        public void Test4()
-        {
-            LoginSteps loginSteps = new LoginSteps(Driver);
-            loginSteps.LogIn();
-
-            var start = DateTime.Now;
-            var element = _waitService.ExistsElement(By.Id("sidebar-projects-add"));
-            var stop = DateTime.Now;
-            Console.Out.WriteLine( stop.Ticks - start.Ticks);
         }
     }
 }
